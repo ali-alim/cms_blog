2ced125da4c2dbf4dfffe6bdca58d14739e7d98a
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-11 16:15:51 +0400

    create GraphQL query from GraphQL CMS

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
 export {default as PostCard} from './PostCard';
 export {default as Categories} from './PostCard';
 export {default as PostWidget} from './PostCard';
+export {default as Header} from './Header';
+export {default as Layout} from './Layout';
---
+++ pages/index.js
@@ -0,0 +1,42 @@
+// import type { NextPage } from 'next'
+import Head from 'next/head'
+import { PostCard, PostWidget, Categories } from '../components'
+import {getPosts} from '../services'
+
+
+
+const Home = ({props}) => {
+  return (
+    <div className="container mx-auto mb-8 px-10">
+      <Head>
+        <title>CMS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            {/* <PostWidget /> */}
+            {/* <Categories /> */}
+          </div>
+          UNKNOWN
+        </div>
+        GRID
+      </div>
+      CONTAINER
+    </div>
+  )
+}
+
+export default Home
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: {posts}
+  }
+}
